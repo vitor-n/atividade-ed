bd82b9e6d1f737ad82cf3c86987f17ca2f5e20c7
GuilhermeCastilho02 <guilherme222castilho@gmail.com>
2024-03-25 08:16:59 -0300

    questão 2

--- atividade.cpp
+++ atividade.cpp
@@ -6,20 +6,40 @@ using std::endl;
 using std::string;
 
 float abs(float& fValor);
+int fatorial(int iValor);
 
 int main() {
     float fValor = -5;
+    int iValor = 3;
     
     float fAbs = abs(fValor);
 
     cout << fAbs << endl;
+    cout << fatorial(iValor) << endl;
+
     return 0;
 }
 
-float abs(float&     fValor) {
+float abs(float& fValor) {
     if (fValor < 0) {
         return -fValor;
     }
     return fValor;
+}
+
+int fatorial(int iValor)
+{   
+    if (iValor < 0)
+    {
+        cout << "O valor nãoo pode ser negativo" << endl;
+        return iValor;
+    }
+
+    if (iValor == 0)
+    {
+        return 1;
+    }
+
+    return iValor * fatorial(iValor - 1);
 }
 
