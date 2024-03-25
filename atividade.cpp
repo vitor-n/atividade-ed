ed417b8b48edac5f0d50399bd2ea38897298e85c
vito0182 <vitor.mnw@gmail.com>
2024-03-25 08:03:31 -0300

    atividade 1

--- atividade.cpp
+++ atividade.cpp
@@ -5,8 +5,21 @@ using std::cout;
 using std::endl;
 using std::string;
 
-int main(){
+float abs(float& fValor);
 
+int main() {
+    float fValor = -5;
+    
+    float fAbs = abs(fValor);
+
+    cout << fAbs << endl;
     return 0;
+}
+
+float abs(float&     fValor) {
+    if (fValor < 0) {
+        return -fValor;
+    }
+    return fValor;
 }
 
